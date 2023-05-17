7b5897dc827675269e826fe22a4a689a2dfa6464
Paul Guyot <pguyot@kallisys.net>
2023-05-17 22:36:34 +0200

    Use taskset to limit to one core

--- test_esp32_nvs_flash_init_wdt.py
+++ test_esp32_nvs_flash_init_wdt.py
@@ -1,6 +1,13 @@
 from pytest_embedded import Dut
 import pytest
 
+@pytest.mark.parametrize(
+    'qemu_prog_path,qemu_cli_args',
+    [
+        ('taskset', '-c 1 qemu-system-xtensa -nographic -machine esp32')
+    ],
+    indirect=True,
+)
 def test_esp32_nvs_flash_init_wdt(dut, redirect):
      dut.expect('calling nvs_flash_init')
      dut.expect('called nvs_flash_init')

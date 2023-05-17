f74966952151b6c7510c40402a3d43ef96f48f84
Paul Guyot <pguyot@kallisys.net>
2023-05-17 08:49:05 +0200

    Remove -nic option

--- test_esp32_nvs_flash_init_wdt.py
+++ test_esp32_nvs_flash_init_wdt.py
@@ -1,13 +1,6 @@
 from pytest_embedded import Dut
 import pytest
 
-@pytest.mark.parametrize(
-    'qemu_extra_args',
-    [
-        '-nic user,model=open_eth',
-    ],
-    indirect=True,
-)
 def test_esp32_nvs_flash_init_wdt(dut, redirect):
      dut.expect('calling nvs_flash_init')
      dut.expect('called nvs_flash_init')

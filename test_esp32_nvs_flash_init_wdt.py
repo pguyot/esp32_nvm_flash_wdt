c1bd17cb9228fd7353eaa81859abecfc85799fb5
Paul Guyot <pguyot@kallisys.net>
2023-05-18 09:49:17 +0200

    Bump dut timeout to 120s

--- test_esp32_nvs_flash_init_wdt.py
+++ test_esp32_nvs_flash_init_wdt.py
@@ -10,4 +10,4 @@ import pytest
 )
 def test_esp32_nvs_flash_init_wdt(dut, redirect):
      dut.expect('calling nvs_flash_init')
-     dut.expect('called nvs_flash_init')
+     dut.expect('called nvs_flash_init', timeout=120)

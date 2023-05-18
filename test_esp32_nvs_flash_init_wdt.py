0edded2fac46ae858f9a30f265e8d9e2c7b3953b
Paul Guyot <pguyot@kallisys.net>
2023-05-18 09:42:05 +0200

    Disable wdt timer emulation

--- test_esp32_nvs_flash_init_wdt.py
+++ test_esp32_nvs_flash_init_wdt.py
@@ -1,6 +1,13 @@
 from pytest_embedded import Dut
 import pytest
 
+@pytest.mark.parametrize(
+    'qemu_extra_args',
+    [
+        '-global driver=timer.esp32.timg,property=wdt_disable,value=true'
+    ],
+    indirect=True
+)
 def test_esp32_nvs_flash_init_wdt(dut, redirect):
      dut.expect('calling nvs_flash_init')
      dut.expect('called nvs_flash_init')
